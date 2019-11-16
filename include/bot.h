33177546dbb0b99a215bb826034da8cafab20054
Chinj17 <chinmaynj17@gmail.com>
2019-11-16 14:59:00 -0500

    Added the include folder containing the bot.h file as per assignment

---
+++ include/bot.h
@@ -0,0 +1,49 @@
+/**
+ * @file bot.h
+ * @brief C++ header file for the Walkerbot class
+ *
+ *
+ * @copyright Copyright (c) Fall 2019 ENPM808X
+ *            This project is released under the BSD 3-Clause License.
+ *
+ * @author Chinmay Joshi
+ *
+ * @date 11-16-2019
+ */
+
+#ifndef INCLUDE_BOT_HPP_
+#define INCLUDE_BOT_HPP_
+
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+/**
+ * @brief Waler class for the turtlebot robot which takes in the
+ *        LaserScan data
+ */
+class roomba {
+private:
+
+  // private member variable to store the obstacle state.
+  bool obs;
+
+public:
+/**
+ * @brief This is the first method of the class. It is a function
+ *        for subscribing to the laser scan data.
+ *
+ * @param Message published by the LaserScan node of turtlebot as input.
+ */
+ void readDistance(const sensor_msgs::LaserScan::ConstPtr&);
+
+/**
+ * @brief This is the second method of the class. It is a function
+ *        which returns if an obstacle is present or not.
+ *
+ * @return Returns if an obstacle is detected or not.
+ */
+ bool obstacle();
+
+};
+
+#endif /* INCLUDE_BOT_HPP_ */
