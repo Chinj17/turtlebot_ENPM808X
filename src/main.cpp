6e241767a86e1fc9e559681f0fdd997280bc6673
Chinj17 <chinmaynj17@gmail.com>
2019-11-16 20:23:57 -0500

    Updated main.cpp from the initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 /**
  * @file main.cpp
- * @brief C++ file for the main node
+ * @brief C++ implementation file for the roomba robot
  *
- * This file contains the implementation of tutorials according to the
- * guidelines given in the assignment Week12_HW taken from ROS Wiki
  *
  * @copyright Copyright (c) Fall 2019 ENPM808X
  *            This project is released under the BSD 3-Clause License.
@@ -12,92 +10,110 @@
  *
  * @date 11-16-2019
  */
-#include <sstream>
-#include "ros/ros.h"
-#include "std_msgs/String.h"
 
+#include <sensor_msgs/LaserScan.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <bot.h>
 
 /**
- * @brief This tutorial demonstrates simple sending of messages
- * over the ROS system.
- * @param argc is the number of inputs
- * @param argv is the input
- * @return integer 0 upon successful execution
+ * This tutorial demonstrates simple obstacle avoidance using turtlebot
  */
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "roomba");
 
-int main(int argc, char **argv) {
-/**
- * The ros::init() function needs to see argc and argv so that it can perform
- * any ROS arguments and name remapping that were provided at the command line.
- * For programmatic remappings you can use a different version of init() which takes
- * remappings directly, but for most command-line programs, passing argc and argv is
- * the easiest way to do it.  The third argument to init() is the name of the node.
- *
- * You must call one of the versions of ros::init() before using any other
- * part of the ROS system.
- */
-  ros::init(argc, argv, "talker");
- /**
-  * NodeHandle is the main access point to communications with the ROS system.
-  * The first NodeHandle constructed will fully initialize this node, and the last
-  * NodeHandle destructed will close down the node.
-  */
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
   ros::NodeHandle n;
 
- /**
-  * The advertise() function is how you tell ROS that you want to
-  * publish on a given topic name. This invokes a call to the ROS
-  * master node, which keeps a registry of who is publishing and who
-  * is subscribing. After this advertise() call is made, the master
-  * node will notify anyone who is trying to subscribe to this topic name,
-  * and they will in turn negotiate a peer-to-peer connection with this
-  * node.  advertise() returns a Publisher object which allows you to
-  * publish messages on that topic through a call to publish().  Once
-  * all copies of the returned Publisher object are destroyed, the topic
-  * will be automatically unadvertised.
-  *
-  * The second parameter to advertise() is the size of the message queue
-  * used for publishing messages.  If messages are published more quickly
-  * than we can send them, the number here specifies how many messages to
-  * buffer up before throwing some away.
-  */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  // Setting default frequency to 10Hz
-  int frequency = 10;
+  // Creating an object of class roomba which will be used for obstacle avoidance
+  roomba walker;
 
-  ros::Rate loop_rate(frequency);
- /**
-  * A count of how many messages we have sent. This is used to create
-  * a unique string for each message.
-  */
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+   auto laserSensor = n.subscribe<sensor_msgs::LaserScan>("/scan", 50,
+                                             &roomba::readDistance, &walker);
 
-  while (ros::ok()) {
- /**
-  * This is a message object. You stuff it with data, and then publish it.
-  */
-  std_msgs::String msg;
+   auto vel = n.advertise<geometry_msgs::Twist>
+                                    ("/mobile_base/commands/velocity", 1000);
 
-  std::stringstream ss;
+  ros::Rate loop_rate(10);
 
-  if (*stringPointer == "") {
-    ROS_ERROR_STREAM("Empty string");
+  geometry_msgs::Twist msg;
+
+  // Adding an intial angle because the orientation of turtlebot was such
+  // that it would have never reached an obstacle
+  msg.angular.z = -0.5;
+
+  /**
+   * A count of how many messages we have sent.
+   */
+
+   int count = 0;
+   while (ros::ok()) {
+     // If obstacle is present
+     if (!walker.obstacle()) {
+       // Move forward
+       msg.linear.x = 0.5;
+
+       // robot should not rotate
+       msg.angular.z = 0.0;
+
+       ROS_INFO_STREAM("Going Forward");
+
+     } else {
+       // If no obstacle is present
+       // Rotate on axis to avoid obstacle
+       msg.angular.z = -0.5;
+
+       // Robot should not move forward while rotating
+       msg.linear.x = 0.0;
+
+       ROS_INFO_STREAM("Obstacle detected! Turning to avoid it");
+     }
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    vel.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
   }
 
-  ss << "Hello World" << count;
-  msg.data = ss.str();
-
-  ROS_INFO("%s", msg.data.c_str());
- /**
-  * The publish() function is how you send messages. The parameter
-  * is the message object. The type of this object must agree with the type
-  * given as a template parameter to the advertise<>() call, as was done
-  * in the constructor above.
-  */
-  chatter_pub.publish(msg);
-
-  ros::spinOnce();
-  loop_rate.sleep();
-  ++count;
-  }
   return 0;
 }
